62706eff527b7de0efd35eff480caaae1e0976b2
Tau <4602612+bash@users.noreply.github.com>
2023-12-09 14:18:11 +0100

    Use monitor, add service

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Install dependencies
+sudo apt-get install -y python3-pyudev python3-gi mplayer
+
+# Enable user linger so that our user service
+# is started on boot and kept around.
+loginctl enable-linger "$USER"
+
+BIN_DIR="$HOME/.local/bin/"
+mkdir -p "$BIN_DIR"
+cp player.py "$BIN_DIR/floppy-player"
+chmod +x "$BIN_DIR/floppy-player"
+
+SERVICES_DIR="$HOME/.config/systemd/user/"
+mkdir -p "$SERVICES_DIR"
+envsubst < floppy-player.service.in > "$SERVICES_DIR/floppy-player.service.in"
+
+# We need our own polkit rule, because filesystem-mount is not allowed
+# by default on raspberry pi os.
+sudo cp etc/polkit-1/rules.d/50-floppy-player.rules /etc/polkit-1/rules.d/
+sudo chmod 644 /etc/polkit-1/rules.d/50-floppy-player.rules
+
+# Enable and start service
+systemctl enable --now --user floppy-player
--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from glob import iglob
 import pyudev
 import subprocess
@@ -9,27 +11,47 @@ from gi.repository import Gio
 from gi.repository.Gio import DBusProxy, BusType, DBusProxyFlags, DBusCallFlags
 from gi.repository.GLib import Variant
 
-# Prerequisites:
-# * pip install pyudev
-# * apt install mplayer
-
 # UDisks2 Docs: http://storaged.org/doc/udisks2-api/2.10.1/
 # pyudev Docs: https://pyudev.readthedocs.io/en/latest/
-# Raspberry Pi OS does not allow users to mount using udisks2,
-# fix polkit policy: https://github.com/coldfix/udiskie/wiki/Permissions but remove if and always return policy.
 
 UDISKS2_BUS = 'org.freedesktop.UDisks2'
 FILE_SYSTEM_IFACE = 'org.freedesktop.UDisks2.Filesystem'
 
-def get_floppy_disk_devices():
-    """Returns an array of all floppy disk devices. Note that the devices may or may not have a floppy disk inserted"""
-    context = pyudev.Context()
-    devices = context.list_devices(subsystem='block', DEVTYPE='disk')
-    return [d for d in devices if is_floppy_disk_device(d)]
+def floppy_player():
+    logging.basicConfig(level=logging.INFO)
+    monitor_floppy_disk_devices(on_floppy_disk_device_changed)
+
+def monitor_floppy_disk_devices(on_device_change):
+    """Calls the provided callback when a floppy disk device is changed."""
+    def _on_device_change(device):
+        if is_floppy_disk_device(device):
+            on_device_change(device)
+
+    monitor = pyudev.Monitor.from_netlink(pyudev.Context())
+    monitor.filter_by(subsystem='block', device_type='disk')
+    observer = pyudev.MonitorObserver(monitor, callback=_on_device_change)
+    observer.daemon = False
+    observer.start()
+
+def on_floppy_disk_device_changed(device):
+    if is_disk_media_change(device) and has_disk_inserted(device):
+        play_audio_files_from_device(device)
+
+def play_audio_files_from_device(device):
+    try:
+        mount_path = mount(device)
+        play_audio_files(mount_path)
+        unmount(device)
+    except e:
+        logging.warn(f'Failed to play from {device.sys_name}: {e}')
 
 def is_floppy_disk_device(device):
     """Tests if the given device is a floppy disk device"""
     return 'ID_TYPE' in device.properties and device.properties['ID_TYPE'] == 'floppy'
+
+def is_disk_media_change(device):
+    """Tests if the media of this device was changed"""
+    return device.action == 'change' and 'DISK_MEDIA_CHANGE' in device.properties and device.properties['DISK_MEDIA_CHANGE'] == '1'
 
 def has_disk_inserted(device):
     """Tests if the given device has a floppy disk inserted"""
@@ -57,15 +79,6 @@ def unmount(device):
     file_system = get_udisk_block_device_as_file_system(device)
     return file_system.call_sync('Unmount', Variant("(a{sv})", ({},)), DBusCallFlags.NONE, -1, None)
 
-def get_mount_points(device):
-    """Gets a list of mount points for the given device"""
-    file_system = get_udisk_block_device_as_file_system(device)
-    return decode_mount_points(file_system.get_cached_property('MountPoints'))
-
-def decode_mount_points(mount_points):
-    # The `MountPoints` property is an array of null-terminated byte arrays
-    return [bytes(m).decode('utf-8').rstrip('\0') for m in mount_points]
-
 def play_audio_files(directory):
     """Plays all audio files in the given directory"""
     for audio_file in iglob('*.mp3', root_dir=directory):
@@ -76,76 +89,5 @@ def play_audio_files(directory):
         except e:
             logging.warn(f'Failed to play audio file {audio_file_path}: {e}')
 
-logging.basicConfig(level=logging.INFO)
-
-played = set()
-# TODO: clear played when device itself is removed
-
-while True:
-    for device in get_floppy_disk_devices():
-        has_disk = has_disk_inserted(device)
-        has_played = device.sys_name in played
-
-        if has_disk and not has_played:
-            logging.info(f'Playing from {device.sys_name}')
-            played.add(device.sys_name)
-            mount_points = get_mount_points(device)
-            mount_path = mount(device) if len(mount_points) == 0 else mount_points[0]
-            play_audio_files(mount_path)
-            unmount(device)
-            logging.info(f'Finished playing from {device.sys_name}. You can remove the disk.')
-
-        if not has_disk:
-            played.discard(device.sys_name)
-
-    time.sleep(5)
-
-# while True:
-#     for device in context.list_devices(subsystem='block', DEVTYPE='disk'):
-#         if 'ID_TYPE' in device.properties and device.properties['ID_TYPE'] == 'floppy':
-#             size = device.attributes.asint('size')
-#             has_floppy_disk_inserted = size > 0
-
-#             if has_floppy_disk_inserted:
-#                 if device.sys_name in played:
-#                     continue
-#             else:
-#                 played.discard(device.sys_name)
-#                 continue
-
-#             print(device.sys_name, device.device_type)
-#             print(device.attributes.asint('size'))
-
-#             block_device = bus.get_object('org.freedesktop.UDisks2', f'/org/freedesktop/UDisks2/block_devices/{device.sys_name}')
-#             file_system = dbus.Interface(block_device, 'org.freedesktop.UDisks2.Filesystem')
-#             properties = dbus.Interface(block_device, dbus.PROPERTIES_IFACE)
-
-#             mount_points = get_mount_points(bus, device)
-#             mount_path = file_system.Mount({}) if len(mount_points) == 0 else mount_points[0]
-#             print(mount_path)
-
-#             for audio_file in iglob('*.mp3', root_dir=mount_path):
-#                 subprocess.check_call(['mplayer', f'{mount_path}/{audio_file}'])
-
-#             played.add(device.sys_name)
-#             print(file_system.Unmount({}))
-
-#             # print(bytes(properties.Get('org.freedesktop.UDisks2.Filesystem', 'MountPoints')))
-
-#             # mount_path = file_system.Mount({})
-#             # print(mount_path)
-            
-#             # print(file_system.Mount({}))
-#             # time.sleep(3)
-#             # print(file_system.Unmount({}))
-
-#             # pprint(file_system)
-
-#             # print(proxy.GetBlockDevices())
-#             # proxy.unmount()
-
-
-#             # pprint({**device})
-#             # udisksctl mount --block-device /dev/sda
-
-#     time.sleep(5)
+if __name__ == '__main__':
+    floppy_player()
